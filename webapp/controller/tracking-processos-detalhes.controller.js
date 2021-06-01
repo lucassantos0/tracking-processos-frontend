b819854cf6354570b8b5ea995e586e1dad817704
wodtke <marcel@wodtke.com.br>
2021-06-01 17:28:20 -0300

    details pages

--- webapp/controller/tracking-processos-detalhes.controller.js
+++ webapp/controller/tracking-processos-detalhes.controller.js
@@ -13,16 +13,7 @@ sap.ui.define([
 
 			},
 			onVoltar: function(oEvent) {
-				var sPreviousHash = History.getInstance().getPreviousHash();
-
-				//The history contains a previous entry
-				if (sPreviousHash !== undefined) {
-					window.history.go(-1);
-				} else {
-					// There is no history!
-					// replace the current hash with page 1 (will not add an history entry)
-					this.getOwnerComponent().getRouter().navTo("tracking-processos-home", null, true);
-				}
+					this.getOwnerComponent().getRouter().navTo("tracking-processos-home");
 			},
 		});
 	});
