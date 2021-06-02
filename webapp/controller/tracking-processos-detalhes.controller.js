0453239978963442b4d74b687dec7b7ee3650d41
wodtke <marcel@wodtke.com.br>
2021-06-02 11:45:48 -0300

    detalhes do sla

--- webapp/controller/tracking-processos-detalhes.controller.js
+++ webapp/controller/tracking-processos-detalhes.controller.js
@@ -1,11 +1,11 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/core/routing/History"
+	"sap/ui/core/Fragment"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller,History) {
+	function (Controller,Fragment) {
 		"use strict";
 
 		return Controller.extend("com.brf.trackingprocessos.trackingprocessosfrontend.controller.tracking-processos-detalhes", {
@@ -15,5 +15,30 @@ sap.ui.define([
 			onVoltar: function(oEvent) {
 					this.getOwnerComponent().getRouter().navTo("tracking-processos-home");
 			},
+
+			onSlaDetail: function (oEvent) {
+				this.openQuickView(oEvent);
+			},
+
+			openQuickView: function (oEvent) {
+
+				var oButton = oEvent.getSource(),
+					oView = this.getView();
+
+				if (!this._pQuickView) {
+					this._pQuickView = Fragment.load({
+						id: oView.getId(),
+						name: "com.brf.trackingprocessos.trackingprocessosfrontend.view.detalhes.sla-detail",
+						controller: this
+					}).then(function (oQuickView) {
+						oView.addDependent(oQuickView);
+						return oQuickView;
+					});
+				}
+				this._pQuickView.then(function (oQuickView){
+					oQuickView.openBy(oButton);
+				});
+			}			
+
 		});
 	});
